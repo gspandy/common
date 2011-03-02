bb1364ff08d314552120352ddd8d1888e7d9e41b
Aaron <aaron@porpoiseltd.com>
2011-03-02 21:15:43 +0000

    formatting

--- src/main/java/com/porpoise/common/collect/Sequences.java
+++ src/main/java/com/porpoise/common/collect/Sequences.java
@@ -2,7 +2,6 @@ package com.porpoise.common.collect;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -43,25 +42,6 @@ public enum Sequences {
         };
     }
 
-    /**
-     * Utility for merging the intersection of two maps.
-     * 
-     * @param <KEY>
-     *            The common key type
-     * @param <VAL1>
-     *            The value type of the first map
-     * @param <VAL2>
-     *            The value type of the second map
-     * @param <RES>
-     *            The value type of the resulting map
-     * @param collate
-     *            a function which can combine the values of map1 (type VAL1) and map2 (type VAL2) and produce a result (type RES)
-     * @param map1
-     *            the first map to merge
-     * @param map2
-     *            the second map to merge
-     * @return a map containing merged values of all the given maps
-     */
     public static <KEY, VAL1, VAL2, RES> Map<KEY, RES> mergeMapsIntersection(final Function<Pair<VAL1, VAL2>, RES> collate, final Map<KEY, VAL1> map1, final Map<KEY, VAL2> map2) {
         final Map<KEY, RES> intersection = Maps.newHashMap();
         for (final KEY key : Sets.intersection(map1.keySet(), map2.keySet())) {
@@ -75,60 +55,11 @@ public enum Sequences {
         return intersection;
     }
 
-    /**
-     * Merges two maps together.
-     * 
-     * @see #mergeMaps(Function, Map, Map, Map, Map...)
-     * 
-     * @param <K>
-     *            the key type
-     * @param <V>
-     *            the value type
-     * @param collate
-     *            the value merging function
-     * @param map1
-     *            the first map to merge with the seoncd
-     * @param map2
-     *            the second map to merge with the first
-     * @return a map containing merged values of all the given maps
-     */
-    public static <K, V> Map<K, V> mergeMaps(final Function<Pair<V, V>, V> collate, final Map<K, V> map1, final Map<K, V> map2) {
-        return mergeMapsInternal(collate, ImmutableList.of(map1, map2));
+    public static <K, V> Map<K, V> mergeMaps(final Map<K, V> map1, final Map<K, V> map2, final Function<Pair<V, V>, V> collate) {
+        return mergeMapsInternal(ImmutableList.of(map1, map2), collate);
     }
 
-    /**
-     * Merges three or more maps. This signature is such so as to differentiate between one which takes two maps.
-     * 
-     * Any keys which appear in multiple maps will have their values merged via the merging function.
-     * 
-     * As the values of the maps may be mutable, be sure to check the function's javadoc to see if any underlying objects will be changed.
-     * 
-     * @param <K>
-     *            The key type
-     * @param <V>
-     *            The value type
-     * @param collate
-     *            A function which can combine map values
-     * @param map1
-     *            the first map to merge
-     * @param map2
-     *            the second map to merge
-     * @param map3
-     *            the third map to merge
-     * @param maps
-     *            the remaining maps to merge the maps to merge
-     * @return a map containing merged values of all the given maps
-     */
-    public static <K, V> Map<K, V> mergeMaps(final Function<Pair<V, V>, V> collate, final Map<K, V> map1, final Map<K, V> map2, final Map<K, V> map3, final Map<K, V>... maps) {
-        final List<Map<K, V>> lists = Lists.newArrayList();
-        lists.add(map1);
-        lists.add(map2);
-        lists.add(map3);
-        lists.addAll(Arrays.asList(maps));
-        return mergeMapsInternal(collate, lists);
-    }
-
-    private static <K, V> Map<K, V> mergeMapsInternal(final Function<Pair<V, V>, V> collate, final List<Map<K, V>> mapsToMerge) {
+    private static <K, V> Map<K, V> mergeMapsInternal(final Iterable<Map<K, V>> mapsToMerge, final Function<Pair<V, V>, V> collate) {
         final Function<Map<K, ?>, Set<K>> getKeys = getKeys();
         final Map<K, V> merged = Maps.newHashMap();
         for (final K key : flatMapSet(mapsToMerge, getKeys)) {
@@ -148,28 +79,10 @@ public enum Sequences {
         return merged;
     }
 
-    /**
-     * flatten all the collections
-     * 
-     * @param <T>
-     *            the collection type
-     * @param all
-     *            a var-arg array of iterables
-     * @return all the collections as one big collection
-     */
     public static <T> Collection<T> flatten(final Iterable<T>... all) {
         return flatten(Arrays.asList(all));
     }
 
-    /**
-     * Convert a matrix (iterable of iterables) into a flattened collection
-     * 
-     * @param <T>
-     *            the collection type
-     * @param all
-     *            the iterable elements
-     * @return the flattened collection
-     */
     public static <T> Collection<T> flatten(final Iterable<? extends Iterable<T>> all) {
         final Collection<T> flat = Lists.newArrayList();
         return addAll(flat, all);
@@ -182,72 +95,22 @@ public enum Sequences {
         return container;
     }
 
-    /**
-     * @param <V>
-     *            the value type
-     * @param <T>
-     *            the comparable type
-     * @param left
-     *            the first object to compare
-     * @param right
-     *            the second object to compare
-     * @return the result after comparing the two values
-     */
     public static <V, T extends Comparable<V>> int compare(final T left, final T right) {
         final Ordering<T> natural = Ordering.natural();
         return natural.nullsFirst().compare(left, right);
     }
 
-    /**
-     * @param <F>
-     *            the source type
-     * @param <T>
-     *            the target type
-     * @param from
-     *            the source collection
-     * @param function
-     *            a function which provides a one-to-many transformation from the source type to many target types
-     * @return a collection of the target type
-     */
     public static <F, T> Collection<T> flatMap(final Iterable<F> from, final Function<? super F, ? extends Iterable<T>> function) {
         final Iterable<Iterable<T>> transformed = Iterables.transform(from, function);
         return flatten(transformed);
     }
 
-    /**
-     * @param <F>
-     *            the source type
-     * @param <T>
-     *            the target type
-     * @param from
-     *            the source collection
-     * @param function
-     *            a function which provides a one-to-many transformation from the source type to many target types
-     * @return a set of the target type
-     */
     public static <F, T> Set<T> flatMapSet(final Iterable<F> from, final Function<? super F, ? extends Iterable<T>> function) {
         final Iterable<Iterable<T>> all = Iterables.transform(from, function);
         final Set<T> flat = Sets.newHashSet();
         return addAll(flat, all);
     }
 
-    /**
-     * If certain that your function will produce only unique values, then this function will return a map for the given collection. If multiple values are returned, then an
-     * {@link IllegalArgumentException} will be thrown
-     * 
-     * see also DomainFunctions.mapById
-     * 
-     * @param collection
-     *            the collection of things to group
-     * @param mapper
-     *            the mapping function
-     * 
-     * @param <K>
-     *            the key type
-     * @param <V>
-     *            the value type
-     * @return a map of values by the key as determined by the function
-     */
     public static <K, V> Map<K, V> groupByUnique(final Iterable<V> collection, final Function<? super V, K> mapper) {
         final Map<K, Collection<V>> map = groupBy(collection, mapper);
         final Function<Iterable<V>, V> onlyElm = new Function<Iterable<V>, V>() {
@@ -260,15 +123,6 @@ public enum Sequences {
         return ImmutableMap.copyOf(Maps.transformValues(map, onlyElm));
     }
 
-    /**
-     * convert the collection into a map which will group the values by the key as returned from the function.
-     * 
-     * @param collection
-     * @param mapper
-     * @param <K>
-     * @param <V>
-     * @return a map of the values by their key
-     */
     public static <K, V> Map<K, Collection<V>> groupBy(final Iterable<V> collection, final Function<? super V, K> mapper) {
         return groupByInternal(collection, mapper);
     }
