335de8206c135a1a65547f1f5803cefab4b578a2
aaron <aaron.pritzlaff@bjss.com>
2011-04-14 16:14:39 +0100

    leaf node

---
+++ src/main/java/com/porpoise/common/collect/tree/LeafNode.java
@@ -0,0 +1,57 @@
+package com.porpoise.common.collect.tree;
+
+import java.util.Collections;
+
+/**
+ * @param <T>
+ */
+public class LeafNode<T> implements TreeNode<T> {
+
+    private final TreeNode<T> parent;
+    private final T data;
+
+    /**
+     */
+    public LeafNode(final T data) {
+        this(null, data);
+    }
+
+    /**
+     * 
+     */
+    public LeafNode(final TreeNode<T> parent, final T data) {
+        this.parent = parent;
+        this.data = data;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.porpoise.common.collect.tree.TreeNode#getParent()
+     */
+    @Override
+    public TreeNode<T> getParent() {
+        return this.parent;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.porpoise.common.collect.tree.TreeNode#getChildren()
+     */
+    @Override
+    public Iterable<? extends TreeNode<T>> getChildren() {
+        return Collections.emptyList();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.porpoise.common.collect.tree.TreeNode#getData()
+     */
+    @Override
+    public T getData() {
+        return this.data;
+    }
+
+}
