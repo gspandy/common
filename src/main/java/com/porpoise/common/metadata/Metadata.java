f6aabc2a9bd9de0c1e4ecf6466b987ee950d3d5f
Aaron Pritzlaff <aaron@porpoiseltd.com>
2011-04-15 21:15:35 +0100

    metadata ismutable

--- src/main/java/com/porpoise/common/metadata/Metadata.java
+++ src/main/java/com/porpoise/common/metadata/Metadata.java
@@ -6,13 +6,26 @@ import com.google.common.base.Function;
  * @param <T>
  */
 public interface Metadata<T> {
-    /**
-     * @return the accessor function
-     */
-    Function<T, ?> accessor();
+	/**
+	 * @return the accessor function
+	 */
+	Function<T, ?> accessor();
 
-    /**
-     * @return the property name
-     */
-    String propertyName();
+	/**
+	 * @return the property name
+	 */
+	String propertyName();
+
+	/**
+	 * @param <V>
+	 * @param input
+	 * @param newValue
+	 * @return
+	 */
+	// <V> boolean update(T input, V newValue);
+
+	/**
+	 * @return true if this metadata can also update its data
+	 */
+	boolean isMutable();
 }
