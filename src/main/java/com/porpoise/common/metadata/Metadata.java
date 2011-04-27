4698061f619a8ff4a0388bd778e07655935caf6c
Aaron Pritzlaff <aaron@porpoiseltd.com>
2011-04-27 13:12:39 +0100

    trim methods

--- src/main/java/com/porpoise/common/metadata/Metadata.java
+++ src/main/java/com/porpoise/common/metadata/Metadata.java
@@ -17,15 +17,20 @@ public interface Metadata<T, V> {
 	String propertyName();
 
 	/**
+	 * This method may not have any effect, or even be supported. If {@link #isMutable()} returns true, then this method
+	 * will return true if the property has changed as a result of this call.
+	 * 
 	 * @param <V>
 	 * @param input
+	 *            The field holder -- an entity with a field represented by this metadata
 	 * @param newValue
+	 *            the new value
 	 * @return true if this method had any affect
 	 */
 	boolean update(T input, V newValue);
 
 	/**
-	 * @return true if this metadata can also update its data
+	 * @return true if the property represented by this field can also update its data
 	 */
 	boolean isMutable();
 }
