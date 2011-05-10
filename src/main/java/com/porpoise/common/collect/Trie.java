967a5abb94002db8c1e558fbe0305f102a8d8d45
Aaron Pritzlaff <aaron@porpoiseltd.com>
2011-05-10 08:24:56 +0100

    trie tests passing

--- src/main/java/com/porpoise/common/collect/Trie.java
+++ src/main/java/com/porpoise/common/collect/Trie.java
@@ -15,6 +15,12 @@ import com.porpoise.common.core.Options;
 import com.porpoise.common.core.Options.Option;
 import com.porpoise.common.strings.StringIterator;
 
+/**
+ * Trie implementation, where each the Trie object is itself a node in the Trie
+ * 
+ * @param <T>
+ *            the data type held in the trie
+ */
 public class Trie<T> implements TreeNode<Option<T>> {
 
     private char                      key;
@@ -50,12 +56,21 @@ public class Trie<T> implements TreeNode<Option<T>> {
     }
 
     public Trie<T> findClosest(final String keyString) {
-        final Trie<T> root = TreeTrait.getRoot(this);
         if (Strings.isNullOrEmpty(keyString)) {
-            return root;
+            return root();
         }
 
-        return closest(new StringIterator(keyString));
+        final StringIterator iter = new StringIterator(keyString);
+        assert iter.hasNext();
+        if (this.key != iter.next().charValue()) {
+            return null;
+        }
+        return closest(iter);
+    }
+
+    private Trie<T> root() {
+        final Trie<T> root = TreeTrait.getRoot(this);
+        return root;
     }
 
     public Option<T> put(final String next, final T leafValue) {
@@ -106,13 +121,16 @@ public class Trie<T> implements TreeNode<Option<T>> {
         final Trie<T> child = Iterables.find(this.children, new Predicate<Trie<T>>() {
             @SuppressWarnings("synthetic-access")
             @Override
-            public boolean apply(final Trie<T> child) {
-                return child.key == charValue;
+            public boolean apply(final Trie<T> input) {
+                return input.key == charValue;
             }
         }, null);
         return child;
     }
 
+    /**
+     * @return the parent trie node
+     */
     @Override
     public Trie<T> getParent() {
         return this.parent;
@@ -123,11 +141,17 @@ public class Trie<T> implements TreeNode<Option<T>> {
         return this.children;
     }
 
+    /**
+     * @return the data held at this node
+     */
     @Override
     public Option<T> getData() {
         return this.value;
     }
 
+    /**
+     * @return the prefix string for this node
+     */
     public String prefix() {
         return append(new StringBuffer()).reverse().toString();
     }
@@ -149,5 +173,20 @@ public class Trie<T> implements TreeNode<Option<T>> {
             }
         };
         return TreeTrait.toString(this, function);
+    }
+
+    /**
+     * @return the longest common prefix for all elements held in this trie
+     */
+    public String longestPrefix() {
+        return root().longestPrefixRecursive();
+    }
+
+    private String longestPrefixRecursive() {
+        if (this.children.size() != 1) {
+            return prefix();
+        }
+        final Trie<T> onlyChild = Iterables.getOnlyElement(this.children, null);
+        return onlyChild.longestPrefixRecursive();
     }
 }
--- src/test/java/com/porpoise/common/collect/TrieTest.java
+++ src/test/java/com/porpoise/common/collect/TrieTest.java
@@ -1,18 +1,29 @@
 package com.porpoise.common.collect;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TrieTest {
 
+    private Trie<Object> trie;
+
+    @Before
+    public void setup() {
+        this.trie = Trie.valueOf("com.acme.one");
+        this.trie.put("com.acme.two");
+        this.trie.put("com.acme.three");
+    }
+
     @Test
     public void testFindClosest() {
-        final Trie<Integer> trie = Trie.valueOf("com.acme.one");
-        trie.put("com.acme.two");
-        trie.put("com.acme.three");
+        Assert.assertEquals("com.acme.", this.trie.findClosest("com.acme.blah").prefix());
+        Assert.assertEquals("com.acme.th", this.trie.findClosest("com.acme.thirteen").prefix());
+        Assert.assertNull(this.trie.findClosest("dave"));
+    }
 
-        System.out.println(trie.toString());
-
-        Assert.assertEquals("com.acme.", trie.findClosest("com.acme.blah").prefix());
+    @Test
+    public void testLongestPrefix() {
+        Assert.assertEquals("com.acme.", this.trie.longestPrefix());
     }
 }
