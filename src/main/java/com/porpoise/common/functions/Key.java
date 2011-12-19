322a7cbdae7a08b14a228cdcecd0377cc59dbeba
Aaron Pritzlaff <aaron@porpoiseltd.com>
2011-12-19 20:41:27 +0000

    guava 10 port

--- src/main/java/com/porpoise/common/functions/Key.java
+++ src/main/java/com/porpoise/common/functions/Key.java
@@ -1,27 +1,33 @@
 package com.porpoise.common.functions;
 
-import com.google.common.base.Equivalence;
 
 /**
- * A Key represents something which is safe to use in a hash or equivalence (tree) set or map. Implementations are expected to be immutable, unless otherwise stated in their javadocs
+ * A Key represents something which is safe to use in a hash or equivalence (tree) set or map. Implementations are
+ * expected to be immutable, unless otherwise stated in their javadocs
  * 
- * A key is largely a marker interface, as any java object already supplies a hashCode and equals method when it overrides {@link Object}.
- *
- * @param <T> The type represented by this key
+ * A key is largely a marker interface, as any java object already supplies a hashCode and equals method when it
+ * overrides {@link Object}.
+ * 
+ * @param <T>
+ *            The type represented by this key
  */
-public interface Key<T> extends Equivalence<T>{
-	
+public interface Key<T> { // extends Equivalence<T>{
+
 	/**
 	 * {@inheritDoc}
+	 * 
 	 * @return the hash code for this key
 	 */
+	@Override
 	public int hashCode();
-	
+
 	/**
 	 * {@inheritDoc}
+	 * 
 	 * @param other
 	 * @return true if this key is equal to the given object
 	 */
+	@Override
 	public boolean equals(Object other);
 
 }
