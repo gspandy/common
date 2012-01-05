20aea547b8928682b5092e25649d959e019938ec
Aaron Pritzlaff <aaron@porpoiseltd.com>
2012-01-05 07:06:49 +0000

    bad test passing

--- src/test/java/com/porpoise/common/files/FilesTest.java
+++ src/test/java/com/porpoise/common/files/FilesTest.java
@@ -15,19 +15,22 @@ import com.google.common.collect.Sets;
  */
 public class FilesTest {
 
-    /**
-     * Test for {@link Files#leafDirs(File)}
-     */
-    @Test
-    public void testLeafDirs() {
-        // call the method under test
-        final Collection<File> leaves = Files.leafDirs(new File(".", "src/main/java"));
-        final Set<String> expected = Sets.newHashSet("tree", "concurrent", "core", "date", "main", "functions", "log",
-                "metadata", "xml", "swing", "strings");
-        for (final File leave : leaves) {
-            Assert.assertTrue(leave.isDirectory());
-            Assert.assertTrue("Unexpected leaf " + leave.getName(), expected.remove(leave.getName()));
-        }
-        Assert.assertTrue(expected.isEmpty());
-    }
+	/**
+	 * Test for {@link Files#leafDirs(File)}
+	 */
+	@Test
+	public void testLeafDirs() {
+		// call the method under test
+		final Collection<File> leaves = Files.leafDirs(new File(".",
+				"src/main/java"));
+		final Set<String> expected = Sets.newHashSet("tree", "concurrent",
+				"core", "date", "main", "functions", "log", "metadata", "xml",
+				"swing", "strings", "annotation");
+		for (final File leave : leaves) {
+			Assert.assertTrue(leave.isDirectory());
+			Assert.assertTrue("Unexpected leaf " + leave.getName(),
+					expected.remove(leave.getName()));
+		}
+		Assert.assertTrue(expected.isEmpty());
+	}
 }
